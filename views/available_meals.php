8ccab296c2d24559615bce0f794e2203d43ae100
Martin Mbithi <martdevelopers254@gmail.com>
2021-10-24 12:28:55 +0300

    Fetch All Meals Under A Certain Category

--- views/available_meals.php
+++ views/available_meals.php
@@ -158,12 +158,12 @@ require_once('../partials/head.php');
                                     <div class="nk-block-head nk-block-head-sm">
                                         <div class="nk-block-between">
                                             <div class="nk-block-head-content">
-                                                <h3 class="nk-block-title page-title">Available Meals In : <?php echo $meal_category->category_name; ?></h3>
+                                                <h3 class="nk-block-title page-title">Available Meals In : <?php echo $meal_category->category_name; ?> Category</h3>
                                                 <div class="nk-block-des text-soft">
                                                     <nav>
                                                         <ul class="breadcrumb breadcrumb-arrow">
                                                             <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
-                                                            <li class="breadcrumb-item"><a href="dashboard">Meal Categories</a></li>
+                                                            <li class="breadcrumb-item"><a href="meal_categories">Meal Categories</a></li>
                                                             <li class="breadcrumb-item active"><?php echo $meal_category->category_name; ?></li>
                                                         </ul>
                                                     </nav>
@@ -172,12 +172,63 @@ require_once('../partials/head.php');
                                         </div><!-- .nk-block-between -->
                                     </div><!-- .nk-block-head -->
                                     <div class="nk-block">
-                                        <div class="card-stretch">
+                                        <div class="card card-bordered card-stretch">
                                             <div class="card-inner-group">
                                                 <div class="card-preview">
                                                     <div class="card-inner">
                                                         <div class="nk-block">
+                                                            <table class="datatable-init nk-tb-list nk-tb-ulist" data-auto-responsive="false">
+                                                                <thead>
+                                                                    <tr class="nk-tb-item nk-tb-head">
+                                                                        <th class="nk-tb-col"><span class="sub-text">Meal Name</span></th>
+                                                                        <th class="nk-tb-col tb-col-md"><span class="sub-text">Meal Price (Ksh)</span></th>
+                                                                        <th class="nk-tb-col nk-tb-col-tools text-right">
+                                                                            <span class="sub-text">Action</span>
+                                                                        </th>
+                                                                    </tr>
+                                                                </thead>
+                                                                <tbody>
+                                                                    <?php
+                                                                    /* Pop This Partial With All Meals */
+                                                                    $ret = "SELECT * FROM meals WHERE meal_category_id = '$view'";
+                                                                    $stmt = $mysqli->prepare($ret);
+                                                                    $stmt->execute(); //ok
+                                                                    $res = $stmt->get_result();
+                                                                    while ($meals = $res->fetch_object()) {
+                                                                    ?>
+                                                                        <tr class="nk-tb-item">
+                                                                            <td class="nk-tb-col">
+                                                                                <div class="user-card">
+                                                                                    <div class="user-avatar bg-dim-primary d-none d-sm-flex">
+                                                                                        <span><?php echo substr($meals->meal_name, 0, 2); ?></span>
+                                                                                    </div>
+                                                                                    <div class="user-info">
+                                                                                        <span class="tb-lead"><?php echo $meals->meal_name; ?></span>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </td>
+                                                                            <td class="nk-tb-col tb-col-md">
+                                                                                <span>Ksh <?php echo $meals->meal_price; ?></span>
+                                                                            </td>
 
+                                                                            <td class="nk-tb-col nk-tb-col-tools">
+                                                                                <ul class="nk-tb-actions gx-1">
+                                                                                    <li>
+                                                                                        <div class="drodown">
+                                                                                            <a href="#" class="dropdown-toggle btn btn-icon btn-trigger" data-toggle="dropdown"><em class="icon ni ni-more-h"></em></a>
+                                                                                            <div class="dropdown-menu dropdown-menu-right">
+                                                                                                <ul class="link-list-opt no-bdr">
+                                                                                                    <li><a href="meal?view=<?php echo $meals->meal_id; ?>"><em class="icon ni ni-focus"></em><span>Meal Details</span></a></li>
+                                                                                                </ul>
+                                                                                            </div>
+                                                                                        </div>
+                                                                                    </li>
+                                                                                </ul>
+                                                                            </td>
+                                                                        </tr>
+                                                                    <?php } ?>
+                                                                </tbody>
+                                                            </table>
                                                         </div>
                                                     </div>
                                                 </div>
