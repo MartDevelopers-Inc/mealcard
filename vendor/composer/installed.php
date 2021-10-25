2b62cbd148365a7aaca7783581bd00dc9becb9da
Martin Mbithi <martdevelopers254@gmail.com>
2021-10-25 14:08:45 +0300

    Install DomPDF - Reports & QR code For Payments & Scaffolded System Details

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => '85b14e631e4417ea0baf7d8ef2f3e67636bceaeb',
+    'reference' => 'a11831d28eefdf0fd9c3b7666d7ff038e352e089',
     'name' => '__root__',
   ),
   'versions' => 
@@ -18,7 +18,34 @@
       'aliases' => 
       array (
       ),
-      'reference' => '85b14e631e4417ea0baf7d8ef2f3e67636bceaeb',
+      'reference' => 'a11831d28eefdf0fd9c3b7666d7ff038e352e089',
+    ),
+    'dompdf/dompdf' => 
+    array (
+      'pretty_version' => 'v1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8768448244967a46d6e67b891d30878e0e15d25c',
+    ),
+    'phenx/php-font-lib' => 
+    array (
+      'pretty_version' => '0.5.2',
+      'version' => '0.5.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ca6ad461f032145fff5971b5985e5af9e7fa88d8',
+    ),
+    'phenx/php-svg-lib' => 
+    array (
+      'pretty_version' => '0.3.4',
+      'version' => '0.3.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f627771eb854aa7f45f80add0f23c6c4d67ea0f2',
     ),
     'phpmailer/phpmailer' => 
     array (
@@ -28,6 +55,33 @@
       array (
       ),
       'reference' => 'dd803df5ad7492e1b40637f7ebd258fee5ca7355',
+    ),
+    'sabberworm/php-css-parser' => 
+    array (
+      'pretty_version' => '8.3.1',
+      'version' => '8.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd217848e1396ef962fb1997cf3e2421acba7f796',
+    ),
+    'tecnickcom/tc-lib-barcode' => 
+    array (
+      'pretty_version' => '1.15.4',
+      'version' => '1.15.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '34b0224f71ebfe83adcef9f14f3d81965fb55493',
+    ),
+    'tecnickcom/tc-lib-color' => 
+    array (
+      'pretty_version' => '1.14.6',
+      'version' => '1.14.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '83cdb57fd900901c6aa2af8cfd67202518fb69b2',
     ),
   ),
 );
