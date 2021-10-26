6f19da0071a0935233e5d3c66b30331a8185c414
Martin Mbithi <martdevelopers254@gmail.com>
2021-10-26 14:33:49 +0300

    Scope Student Module

--- partials/student_aside.php
+++ partials/student_aside.php
@@ -6,75 +6,33 @@
                 <h6 class="overline-title">Menu</h6>
             </li>
             <li class="nk-menu-item">
-                <a href="html/subscription/index.html" class="nk-menu-link">
+                <a href="home" class="nk-menu-link">
                     <span class="nk-menu-icon"><em class="icon ni ni-dashboard"></em></span>
                     <span class="nk-menu-text">Dashboard</span>
                 </a>
             </li>
             <li class="nk-menu-item">
-                <a href="html/subscription/subscriptions.html" class="nk-menu-link">
-                    <span class="nk-menu-icon"><em class="icon ni ni-file-text"></em></span>
-                    <span class="nk-menu-text">My Subscription</span>
+                <a href="my_meal_card" class="nk-menu-link">
+                    <span class="nk-menu-icon"><em class="icon ni ni-cc-secure"></em></span>
+                    <span class="nk-menu-text">Meal Card</span>
                 </a>
             </li>
             <li class="nk-menu-item">
-                <a href="html/subscription/payments.html" class="nk-menu-link">
+                <a href="my_meals" class="nk-menu-link">
+                    <span class="nk-menu-icon"><em class="icon ni ni-coffee"></em></span>
+                    <span class="nk-menu-text">Meals</span>
+                </a>
+            </li>
+            <li class="nk-menu-item">
+                <a href="my_orders" class="nk-menu-link">
+                    <span class="nk-menu-icon"><em class="icon ni ni-clipboad-check"></em></span>
+                    <span class="nk-menu-text">My Orders</span>
+                </a>
+            </li>
+            <li class="nk-menu-item">
+                <a href="my_payments" class="nk-menu-link">
                     <span class="nk-menu-icon"><em class="icon ni ni-report-profit"></em></span>
-                    <span class="nk-menu-text">Payment History</span>
-                </a>
-            </li>
-            <li class="nk-menu-item">
-                <a href="html/subscription/downloads.html" class="nk-menu-link">
-                    <span class="nk-menu-icon"><em class="icon ni ni-download-cloud"></em></span>
-                    <span class="nk-menu-text">Download</span>
-                </a>
-            </li>
-            <li class="nk-menu-item">
-                <a href="html/subscription/profile-setting.html" class="nk-menu-link">
-                    <span class="nk-menu-icon"><em class="icon ni ni-account-setting"></em></span>
-                    <span class="nk-menu-text">Account Setting</span>
-                </a>
-            </li>
-            <li class="nk-menu-heading">
-                <h6 class="overline-title">See Others</h6>
-            </li>
-            <li class="nk-menu-item">
-                <a href="#" class="nk-menu-link">
-                    <span class="nk-menu-icon"><em class="icon ni ni-files"></em></span>
-                    <span class="nk-menu-text">Pre-Built Pages</span>
-                </a>
-                <ul class="nk-menu-sub">
-                    <li class="nk-menu-item"><a href="html/subscription/pricing.html" class="nk-menu-link"><span class="nk-menu-text">Pricing</span></a></li>
-                    <li class="nk-menu-item"><a href="html/subscription/team-access.html" class="nk-menu-link"><span class="nk-menu-text">Team Access</span></a></li>
-                    <li class="nk-menu-item"><a href="html/subscription/profile-activity.html" class="nk-menu-link"><span class="nk-menu-text">Login Activity</span></a></li>
-                    <li class="nk-menu-item"><a href="html/subscription/invoices.html" class="nk-menu-link"><span class="nk-menu-text">Invoices <span class="count">(2)</span></span></a></li>
-                    <li class="nk-menu-item"><a href="html/subscription/invoice-details.html" class="nk-menu-link"><span class="nk-menu-text">View Invoice</span></a></li>
-                    <li class="nk-menu-item"><a href="html/subscription/tickets.html" class="nk-menu-link"><span class="nk-menu-text">Tickets <span class="count">(5)</span></span></a></li>
-                    <li class="nk-menu-item"><a href="html/subscription/ticket-details.html" class="nk-menu-link"><span class="nk-menu-text">Ticket Details</span></a></li>
-                    <li class="nk-menu-item"><a href="html/subscription/support.html" class="nk-menu-link"><span class="nk-menu-text">Support</span></a></li>
-                    <li class="nk-menu-item"><a href="html/subscription/subscriptions-detail.html" class="nk-menu-link"><span class="nk-menu-text">Subscription Details</span></a></li>
-                </ul>
-            </li>
-        </ul>
-        <!-- Menu -->
-        <ul class="nk-menu nk-menu-sm">
-            <!-- Menu -->
-            <li class="nk-menu-heading">
-                <span>Help Center</span>
-            </li>
-            <li class="nk-menu-item">
-                <a href="html/subscription/faqs.html" class="nk-menu-link">
-                    <span class="nk-menu-text">FAQs</span>
-                </a>
-            </li>
-            <li class="nk-menu-item">
-                <a href="html/subscription/contact.html" class="nk-menu-link">
-                    <span class="nk-menu-text">Contact</span>
-                </a>
-            </li>
-            <li class="nk-menu-item">
-                <a href="html/subscription/support.html" class="nk-menu-link">
-                    <span class="nk-menu-text">Support</span>
+                    <span class="nk-menu-text">My Payments</span>
                 </a>
             </li>
         </ul>
