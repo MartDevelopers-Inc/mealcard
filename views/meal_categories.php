0e58d41b5d28c14deac362be3c87c8206bea5bf9
Martin Mbithi <martdevelopers254@gmail.com>
2021-11-28 15:52:18 +0300

    Refactor Delete GET Method To Post - On Meal Categories

--- views/meal_categories.php
+++ views/meal_categories.php
@@ -115,14 +115,14 @@ if (isset($_POST['update_meal_category'])) {
 }
 
 /* Handle Delete Meal Category */
-if (isset($_GET['delete'])) {
-    $delete = $_GET['delete'];
+if (isset($_POST['delete'])) {
+    $category_id = $_POST['category_id'];
     /* Wipe This MF */
-    $delete_sql = "DELETE FROM meal_categories WHERE category_id = '$delete'";
+    $delete_sql = "DELETE FROM meal_categories WHERE category_id = '$category_id'";
     $delete_sql_stmt = $mysqli->prepare($delete_sql);
     $delete_sql_stmt->execute();
     if ($delete_sql_stmt) {
-        $success = "Deleted" && header("refresh:1; url=meal_categories");
+        $success = "Category Details Deleted";
     } else {
         $err  = "Failed!, Please Try Again Later";
     }
